26d1e82ea484b7ab189bd1846bfeb7d2f3a59dd1
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-11-16 15:59:22 -0800

    add initial statelessWidget with appBar and container for body

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
+      MaterialApp(home: BallPage()),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Center(child: Text('Ask Me Anything')),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(),
+    );
+  }
+}
