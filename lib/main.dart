9ebc09a804798527fc53cef8b305ab709537fe80
Harriet Thompson <hazthomps@yahoo.co.uk>
2019-11-16 16:56:30 -0800

    add randomise functionality on press of 8 ball image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(home: BallPage()),
@@ -11,9 +12,36 @@ class BallPage extends StatelessWidget {
       backgroundColor: Colors.lightBlue,
       appBar: AppBar(
         title: Center(child: Text('Ask Me Anything')),
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.blueAccent.shade100,
       ),
-      body: Container(),
+      body: Ball(),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 4;
+
+  changeBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      print('clicked');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: FlatButton(
+      onPressed: () {
+        changeBallNumber();
+      },
+      child: Image.asset('images/ball$ballNumber.png'),
+    ));
   }
 }
